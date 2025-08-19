c4684caea7f798cb291b6b38627aa88a443b01ac
Solosmith <arthurpassos05@gmail.com>
2025-08-19 11:58:03 -0300

    Transacao finalized

--- src/Negocios/Transacao.java
+++ src/Negocios/Transacao.java
@@ -1,4 +1,33 @@
 package Negocios;
 
+import java.time.LocalDate;
+
 public class Transacao {
+    private String id;
+    private String descricao;
+    private Double valor;
+    private LocalDate data;
+
+    public Transacao(String id, String descricao, Double valor, LocalDate data) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
 }
