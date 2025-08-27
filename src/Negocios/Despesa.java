4b0053c9d1172d7efdcad1d61b99db804709bc22
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-27 01:19:03 -0300

    Despesa.java
    add o parametro 'categoria' e mexi no  valor pra garantir q as receitas smp sejam positivas e as despesas negativa
    
    so isso nos 2 codigos

--- src/Negocios/Despesa.java
+++ src/Negocios/Despesa.java
@@ -2,8 +2,9 @@ package Negocios;
 
 import java.time.LocalDate;
 
-public class Despesa extends Transacao{
-    public Despesa(String id, String descricao, double valor, LocalDate data,Conta conta) {
-        super(id, descricao, valor, data, conta);
+public class Despesa extends Transacao {
+    
+    public Despesa(String id, String descricao, double valor, LocalDate data, Categoria categoria, Conta conta) {
+        super(id, descricao, -Math.abs(valor), data, categoria, conta);
     }
 }
