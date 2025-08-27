0792c1dfc0e8be360bcb1cff3d5d6eb615dd2bb2
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-27 01:19:26 -0300

    Receita.java

--- src/Negocios/Receita.java
+++ src/Negocios/Receita.java
@@ -2,8 +2,9 @@ package Negocios;
 
 import java.time.LocalDate;
 
-public class Receita extends Transacao{
-    public Receita(String id, String descricao, double valor, LocalDate data, Conta conta) {
-        super(id, descricao, valor, data, conta);
+public class Receita extends Transacao {
+    
+    public Receita(String id, String descricao, double valor, LocalDate data, Categoria categoria, Conta conta) {
+        super(id, descricao, Math.abs(valor), data, categoria, conta);
     }
 }
