99d949c5b903966b7701019f5105dc9ffd117194
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-27 01:12:28 -0300

    Transacao.java
    add o atributo conta do tipo CONTA  pra permitir q as transações sejam ligadas as conta bancaria
    
    mexi no construtor para incluir 'conta' e 'categoria', pra q td transação tenha esses dados obrigatorios
    
    e ajustei  tostring para mostrar o nome da categoria e da conta

--- src/Negocios/Transacao.java
+++ src/Negocios/Transacao.java
@@ -1,69 +1,84 @@
 package Negocios;
 
-import java.time.LocalDate;	
+import java.time.LocalDate;
 import java.util.Objects;
-import Negocios.Conta;
+import java.io.Serializable;
 
-public abstract class Transacao {
-    private String id;
-    private String descricao;
-    private Double valor;
-    private LocalDate data;
-    private Categoria categoria;
-    private Conta conta;
+public abstract class Transacao implements Serializable {
+	private String id;
+	private String descricao;
+	private Double valor;
+	private LocalDate data;
+	private Categoria categoria;
+	private Conta conta;
 
-    public Transacao(String id, String descricao, Double valor, LocalDate data, Conta conta) {
-        this.id = id;
-        this.descricao = descricao;
-        this.valor = valor;
-        this.data = data;
-        this.conta = conta;
-    }
-    public void setDescricao(String descricao) {
-    	this.descricao = descricao;
-    }
-    
-    public void setValor(double valor) {
-    	this.valor = valor;
-    }
-    
-    public void setData(LocalDate data) {
-    	this.data = data;
-    }
-    
-    public void setValor(Double valor) {
-		this.valor = valor;
+
+	public Transacao(String id, String descricao, Double valor, LocalDate data, Categoria categoria, Conta conta) {
+		this.id=id;
+		this.descricao=descricao;
+		this.valor=valor;
+		this.data=data;
+		this.categoria=categoria;
+		this.conta=conta;
 	}
-	public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
 
-    public String getId() {
-        return id;
-    }
+	public String getId(){
+		return id;
+	}
+	public String getDescricao(){
+		return descricao;
+	}
+	public Double getValor (){
+		return valor;
+	}
+	public LocalDate getData(){
+		return data;
+	}
+	public Categoria getCategoria(){
+		return categoria;
+	}
+	public Conta getConta(){
+		return conta;
+	}
 
-    public String getDescricao() {
-        return descricao;
-    }
+	public void setDescricao (String descricao){
+		this.descricao=descricao;
+	}
+	public void setValor ( Double valor ){
+		this.valor=valor;
+	}
+	public void setData ( LocalDate data){
+		this.data=data;
+	}
+	public void setCategoria ( Categoria categoria){
+		this.categoria=categoria;
+	}
+	public void setConta (Conta conta ){
+		this.conta=conta;
+	}
+//comparar pra ver se o id é unico
 
-    public Double getValor() {
-        return valor;
-    }
+	@Override
+	public boolean equals (Object o ){
+		if (this == o) return true;
+		if (!(o instanceof Transacao outra)) return false;
+		return Objects.equals(id, outra.id);
+	}
 
-    public LocalDate getData() {
-        return data;
-    }
+	@Override
+	public int hashCode (){
+		return Objects.hash (id);
+	}
 
-    public Categoria getCategoria() { return categoria; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Transacao outra)) return false;
-        return Objects.equals(id, outra.id);
+	@Override
+	public String toString(){
+		return "Transacao{" +
+               "id='" + id + '\'' +
+               ", descricao='" + descricao + '\'' +
+               ", valor=" + valor +
+               ", data=" + data +
+               ", categoria=" + (categoria != null ? categoria.getNome() : "N/A") +
+               ", conta=" + (conta != null ? conta.getNome() : "N/A") +
+               '}';
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-}
+} 
