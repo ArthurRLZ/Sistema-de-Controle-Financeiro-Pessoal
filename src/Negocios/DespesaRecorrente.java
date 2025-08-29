c136d191f6d3386edd43926dd6ca90283c9d2bec
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 04:35:33 -0300

    Update DespesaRecorrente.java

--- src/Negocios/DespesaRecorrente.java
+++ src/Negocios/DespesaRecorrente.java
@@ -2,77 +2,86 @@ package Negocios;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.Objects;
 
 public class DespesaRecorrente extends Despesa implements Serializable {
     private static final long serialVersionUID = 1L;
-    
-    private String frequencia;
-    private int numeroDeParcelas;
-    private LocalDate dataUltimaCobranca;
 
-    public DespesaRecorrente(String id, String descricao, double valor, LocalDate data, Categoria categoria, Conta conta, String frequencia, int numeroDeParcelas) {
-        super(id, descricao, valor, data, categoria, conta);
-        this.frequencia = frequencia;
+    private Periodicidade periodicidade;
+    private int numeroDeParcelas;
+    private int parcelaAtual;
+    private LocalDate proximaOcorrencia;
+
+    // Construtor
+    public DespesaRecorrente(double valor, Date data, String descricao, Conta conta, Categoria categoria, Periodicidade periodicidade, int numeroDeParcelas) {
+        super(valor, data, descricao, conta, categoria);
+        this.periodicidade = periodicidade;
         this.numeroDeParcelas = numeroDeParcelas;
-        this.dataUltimaCobranca = data;
+        this.parcelaAtual = 0;
+        this.proximaOcorrencia = LocalDate.now();
+        atualizarParaProximaOcorrencia(); // Calcula a primeira ocorrência
     }
 
-    public String getFrequencia() {
-        return frequencia;
+    // Métodos de Negócio
+    public boolean isAtiva() {
+        return this.parcelaAtual < this.numeroDeParcelas;
+    }
+
+    public boolean isVencida() {
+        return isAtiva() && !proximaOcorrencia.isAfter(LocalDate.now());
+    }
+
+    public final void atualizarParaProximaOcorrencia() {
+        if (isAtiva()) {
+            this.parcelaAtual++;
+
+            LocalDate proxima = this.proximaOcorrencia;
+            switch (this.periodicidade) {
+                case DIARIA:
+                    this.proximaOcorrencia = proxima.plusDays(1);
+                    break;
+                case SEMANAL:
+                    this.proximaOcorrencia = proxima.plusWeeks(1);
+                    break;
+                case MENSAL:
+                    this.proximaOcorrencia = proxima.plusMonths(1);
+                    break;
+                case ANUAL:
+                    this.proximaOcorrencia = proxima.plusYears(1);
+                    break;
+            }
+        }
+    }
+
+    // Getters
+    public Periodicidade getPeriodicidade() {
+        return periodicidade;
     }
 
     public int getNumeroDeParcelas() {
         return numeroDeParcelas;
     }
 
-    public LocalDate getDataUltimaCobranca() {
-        return dataUltimaCobranca;
+    public int getParcelaAtual() {
+        return parcelaAtual;
     }
 
-    public void setFrequencia(String frequencia) {
-        this.frequencia = frequencia;
+    public LocalDate getProximaOcorrencia() {
+        return proximaOcorrencia;
     }
-
-    public void setNumeroDeParcelas(int numeroDeParcelas) {
-        this.numeroDeParcelas = numeroDeParcelas;
-    }
-
-    public void setDataUltimaCobranca(LocalDate dataUltimaCobranca) {
-        this.dataUltimaCobranca = dataUltimaCobranca;
-    }
-
-    // calcula a data da proxima cobranca com base na frequencia
-    public LocalDate calcularProximaData() {
-        LocalDate proximaData = this.dataUltimaCobranca;
-        switch (frequencia.toLowerCase()) {
-            case "mensal":
-                proximaData = proximaData.plusMonths(1);
-                break;
-            case "trimestral":
-                proximaData = proximaData.plusMonths(3);
-                break;
-            case "semestral":
-                proximaData = proximaData.plusMonths(6);
-                break;
-            case "anual":
-                proximaData = proximaData.plusYears(1);
-                break;
-        }
-        return proximaData;
-    }
-
+    
+   
     @Override
     public String toString() {
-        return "Despesa Recorrente{" +
-               "id='" + this.getId() + '\'' +
-               ", descricao='" + this.getDescricao() + '\'' +
-               ", valor=" + this.getValor() +
-               ", data=" + this.getData() +
-               ", categoria=" + this.getCategoria().getNome() +
-               ", conta=" + this.getConta().getNome() +
-               ", frequencia='" + frequencia + '\'' +
-               ", numeroDeParcelas=" + numeroDeParcelas +
-               ", proximaGeracao=" + this.calcularProximaData() +
-               '}';
+        return "DespesaRecorrente{" +
+                "id=" + getId() +
+                ", descricao='" + getDescricao() + '\'' +
+                ", valor=" + getValor() +
+                ", periodicidade=" + periodicidade +
+                ", parcela=" + parcelaAtual + "/" + numeroDeParcelas +
+                ", proximaOcorrencia=" + proximaOcorrencia +
+                ", conta=" + getConta().getNome() +
+                '}';
     }
 }
