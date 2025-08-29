1c3fa9eac043507001d858718159f0bd8a1d622c
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 04:39:08 -0300

    Update Despesa.java

--- src/Negocios/Despesa.java
+++ src/Negocios/Despesa.java
@@ -1,10 +1,30 @@
 package Negocios;
 
-import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Date;
 
-public class Despesa extends Transacao {
+public class Despesa extends Transacao implements Serializable {
+    private static final long serialVersionUID = 1L;
     
-    public Despesa(String id, String descricao, double valor, LocalDate data, Categoria categoria, Conta conta) {
-        super(id, descricao, -Math.abs(valor), data, categoria, conta);
+    private Categoria categoria;
+
+    // Construtor
+    public Despesa(double valor, Date data, String descricao, Conta conta, Categoria categoria) {
+        super(valor, data, descricao, conta); // Passa as informações comuns para a classe-mãe
+        this.categoria = categoria;
+    }
+
+    // Getter
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    // Método Padrão
+    @Override
+    public String toString() {
+        return "Despesa{" +
+                super.toString() + // Chama o toString() da classe Transacao
+                ", categoria=" + categoria.getNome() +
+                '}';
     }
 }
