d991c707c381fa61f4b02daac95c87754764939f
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 04:37:32 -0300

    Update Transacao.java

--- src/Negocios/Transacao.java
+++ src/Negocios/Transacao.java
@@ -1,86 +1,76 @@
 package Negocios;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
-import java.io.Serializable;
+import java.util.Date; // Usaremos o java.util.Date por compatibilidade
 
 public abstract class Transacao implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private String id;
-	private String descricao;
-	private Double valor;
-	private LocalDate data;
-	private Categoria categoria;
-	private Conta conta;
+    private static final long serialVersionUID = 1L;
 
+    private int id;
+    private double valor;
+    private Date data;
+    private String descricao;
+    private Conta conta;
 
-	public Transacao(String id, String descricao, Double valor, LocalDate data, Categoria categoria, Conta conta) {
-		this.id=id;
-		this.descricao=descricao;
-		this.valor=valor;
-		this.data=data;
-		this.categoria=categoria;
-		this.conta=conta;
-	}
-
-	public String getId(){
-		return id;
-	}
-	public String getDescricao(){
-		return descricao;
-	}
-	public Double getValor (){
-		return valor;
-	}
-	public LocalDate getData(){
-		return data;
-	}
-	public Categoria getCategoria(){
-		return categoria;
-	}
-	public Conta getConta(){
-		return conta;
-	}
-
-	public void setDescricao (String descricao){
-		this.descricao=descricao;
-	}
-	public void setValor ( Double valor ){
-		this.valor=valor;
-	}
-	public void setData ( LocalDate data){
-		this.data=data;
-	}
-	public void setCategoria ( Categoria categoria){
-		this.categoria=categoria;
-	}
-	public void setConta (Conta conta ){
-		this.conta=conta;
-	}
-//comparar pra ver se o id é unico
-
-	@Override
-	public boolean equals (Object o ){
-		if (this == o) return true;
-		if (!(o instanceof Transacao outra)) return false;
-		return Objects.equals(id, outra.id);
-	}
-
-	@Override
-	public int hashCode (){
-		return Objects.hash (id);
-	}
-
-	@Override
-	public String toString(){
-		return "Transacao{" +
-               "id='" + id + '\'' +
-               ", descricao='" + descricao + '\'' +
-               ", valor=" + valor +
-               ", data=" + data +
-               ", categoria=" + (categoria != null ? categoria.getNome() : "N/A") +
-               ", conta=" + (conta != null ? conta.getNome() : "N/A") +
-               '}';
+    // Construtor
+    public Transacao(double valor, Date data, String descricao, Conta conta) {
+        this.valor = valor;
+        this.data = data;
+        this.descricao = descricao;
+        this.conta = conta;
     }
-} 
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { // Usado pelo repositório
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao) { // Permitimos editar a descrição
+        this.descricao = descricao;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    // Métodos Padrão
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transacao transacao = (Transacao) o;
+        return id == transacao.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+    @Override
+    public String toString() {
+        return  "id=" + id +
+                ", valor=" + valor +
+                ", data=" + data +
+                ", descricao='" + descricao + '\'' +
+                ", conta=" + conta.getNome();
+    }
+}
