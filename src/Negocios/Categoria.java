e5f94552a93773a8f24c43271ed8f1062f173712
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 05:17:49 -0300

    Update Categoria.java

--- src/Negocios/Categoria.java
+++ src/Negocios/Categoria.java
@@ -5,41 +5,35 @@ import java.io.Serializable;
 
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
-    
-    private String id;
+    private int id;
     private String nome;
 
-    public Categoria(String id, String nome) {
-        this.id = id;
+    public Categoria(String nome) {
         this.nome = nome;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
         return nome;
     }
 
-
-    // adicionei esse método pra gente poder mudar o nome da categoria dps
-    // tp se o cara errar ou quiser trocar o nome tlgd
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    // esse  equals é pra garantir qua gnt não va ter 2 categorias com o mesmo ID
-    // ele so compara o ID então se o ID for igual  ele considera que são a mesma categoria
-
-
-	// equals baseado no ID
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Categoria outra)) return false;
-        return Objects.equals(id, outra.id);
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id == categoria.id;
     }
 
     @Override
@@ -47,14 +41,8 @@ public class Categoria implements Serializable {
         return Objects.hash(id);
     }
 
-    // mudei o 'toString' pra ficar mais izi de ver as info de uma categoria
-    // mas ve se ta certo essa bomba ai e qlqr coisa mexe ai nessa bosta
-
     @Override
     public String toString() {
-        return "Categoria{" +
-               "id='" + id + '\'' +
-               ", nome='" + nome + '\'' +
-               '}';
+        return "Categoria{" + "id=" + id + ", nome='" + nome + '\'' + '}';
     }
 }
