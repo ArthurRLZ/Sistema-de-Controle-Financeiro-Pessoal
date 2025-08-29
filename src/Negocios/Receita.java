aaa75f82c56b6caa84f87e660125d4f03c456c2e
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 04:39:43 -0300

    Update Receita.java

--- src/Negocios/Receita.java
+++ src/Negocios/Receita.java
@@ -1,10 +1,21 @@
 package Negocios;
 
-import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Date;
 
-public class Receita extends Transacao {
-    
-    public Receita(String id, String descricao, double valor, LocalDate data, Categoria categoria, Conta conta) {
-        super(id, descricao, Math.abs(valor), data, categoria, conta);
+public class Receita extends Transacao implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // Construtor
+    public Receita(double valor, Date data, String descricao, Conta conta) {
+        super(valor, data, descricao, conta); // Apenas chama o construtor da classe-mãe
+    }
+
+    // Método Padrão
+    @Override
+    public String toString() {
+        return "Receita{" +
+                super.toString() + // Chama o toString() da classe Transacao
+                '}';
     }
 }
