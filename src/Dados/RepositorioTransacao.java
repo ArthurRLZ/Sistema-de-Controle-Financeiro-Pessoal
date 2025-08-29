38c132da889b3f6ac00cd10075bd3f1e0fd7f440
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 03:08:34 -0300

    RepositorioTransacao.java

--- src/Dados/RepositorioTransacao.java
+++ src/Dados/RepositorioTransacao.java
@@ -1,15 +1,36 @@
 package Dados;
 
 import Negocios.Transacao;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RepositorioTransacao {
-    private ArrayList<Transacao> transacoes = new ArrayList<>();
+public class RepositoriTransacao implements Serializable {
+    private ArrayList<Transacao> transacoes;
 
-    public void adicionar(Transacao t) { transacoes.add(t); }
-    public boolean remover(String id) {
-        return transacoes.removeIf(t -> t.getId().equals(id));
+    public RepositorioTransacao() {
+        this.transacoes = new ArrayList <>();
     }
-    public ArrayList<Transacao> getTodos() { return transacoes; }
+
+    public void adicionar (Transacao t) {
+        if (t != null && !transacoes.contains(t)) {
+            transacoes.add(t);
+        }
+    }
+
+    public boolean remover (String id) {
+        return transacoes.removeIf ( t-> t.getId().equals(id));
+    }
+
+    public Transacao buscarPorId(String id) {
+        for ( Transacao t : transacoes ) {
+            if (t.getId().equals(id)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public ArratList<Transacao> getTodos () {
+        return transacoes;
+    }
 }
