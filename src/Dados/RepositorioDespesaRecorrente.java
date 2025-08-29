c3c82fd083ff625ce2d448f7939d2f6fd7550534
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 05:31:09 -0300

    Update RepositorioDespesaRecorrente.java

--- src/Dados/RepositorioDespesaRecorrente.java
+++ src/Dados/RepositorioDespesaRecorrente.java
@@ -15,7 +15,27 @@ public class RepositorioDespesaRecorrente implements Serializable {
         this.despesasRecorrentes.add(despesa);
     }
 
+    public boolean remover(int id) {
+        return this.despesasRecorrentes.removeIf(dr -> dr.getId() == id);
+    }
+
+    public DespesaRecorrente buscarPorId(int id) {
+        for (DespesaRecorrente dr : this.despesasRecorrentes) {
+            if (dr.getId() == id) {
+                return dr;
+            }
+        }
+        return null;
+    }
+
     public List<DespesaRecorrente> listarTodas() {
         return new ArrayList<>(this.despesasRecorrentes);
+    }
+
+    public void setDespesasRecorrentes(List<DespesaRecorrente> despesas) {
+        this.despesasRecorrentes = despesas;
+        if (!despesas.isEmpty()) {
+            this.proximoId = despesas.stream().mapToInt(DespesaRecorrente::getId).max().orElse(0) + 1;
+        }
     }
 }
