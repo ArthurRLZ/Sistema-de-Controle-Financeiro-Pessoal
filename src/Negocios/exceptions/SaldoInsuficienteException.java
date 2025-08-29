a65859d9857bb7907077e8dd464db501cb66368d
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 04:26:33 -0300

    Create SaldoInsuficienteException.java

---
+++ src/Negocios/exceptions/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package Negocios.exceptions;
+
+public class SaldoInsuficienteException extends NegocioException {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}
