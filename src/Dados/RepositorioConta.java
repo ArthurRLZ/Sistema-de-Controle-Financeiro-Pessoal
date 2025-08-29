18d2204a02644a1f7687107c70a7b805db5d9ae6
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 03:01:29 -0300

    RepositorioConta.java

--- src/Dados/RepositorioConta.java
+++ src/Dados/RepositorioConta.java
@@ -1,29 +1,37 @@
 package Dados;
 
 import Negocios.Conta;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RepositorioConta {
-    private ArrayList<Conta> contas = new ArrayList<>();
+public class RepositorioConta implements Serializable {
 
-    public void adicionar(Conta c) { 
-        contas.add(c); 
+    private ArrayList<Conta> contas;
+
+    public RepositorioConta() {
+        this.contas = new ArrayList<>();
     }
-    
+
+    public void adicionar (Conta c){
+        if (c != null && !contas.contains(c)) {
+            contas.add(c);
+        }
+    }
+
     public boolean remover(String id) {
-        return contas.removeIf(c -> c.getId().equals(id));
+        return contas.removeIf( c -> c.getId().equals(id));
     }
-    
-    public Conta buscarPorId(String id) {
-        for (Conta c : contas) {
-            if (c.getId().equals(id)) {
+
+    public Conta buscarPorId(String id){
+        for( Conta c : contas) {
+            if (c.getId().equals(id)){
                 return c;
             }
         }
-        return null; 
+        return null;
     }
 
-    public ArrayList<Conta> getTodos() { 
-        return contas; 
+    public ArrayList<Conta> getTodos() {
+        return contas;
     }
 }
