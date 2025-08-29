19aad191f923e4575f45f493bc728cf968e8aa7d
Euclides Laurindo <euclides.emanuel@hotmail.com>
2025-08-29 02:53:03 -0300

    RepositorioCategoria.java

--- src/Dados/RepositorioCategoria.java
+++ src/Dados/RepositorioCategoria.java
@@ -1,28 +1,35 @@
 package Dados;
 
 import Negocios.Categoria;
-import Negocios.Conta;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RepositorioCategoria {
-    private ArrayList<Categoria> categorias = new ArrayList<>();
+public class RepositorioCategoria implements Serializable {
+    private ArrayList<Categoria> categorias;
 
-    public void adicionar(Categoria c) { 
-    	categorias.add(c); 
+    public RepositorioCategoria(){
+        this.categorias = new ArrayList<>();
     }
-    public boolean remover(String id) {
-        return categorias.removeIf(c -> c.getId().equals(id));
+    public void adicionar ( Categoria c) {
+        if ( c != null && !categorias.contains(c)) {
+            categorias.add(c);
+        }
     }
-    public ArrayList<Categoria> getTodos() { 
-    	return categorias; 
-    	}
-    public Categoria buscarPorId(String id) {
-        for (Categoria c : categorias) {
-            if (c.getId().equals(id)) {
+
+    public boolean remover ( String id) {
+        return categorias.removeIf(c-> c.getId(). equals(id));
+    }
+
+    public Categoria buscarPorId(String id){
+        for ( Categoria c : categorias ) {
+            if (c.getId().equals(id)){
                 return c;
             }
         }
-        return null; 
+        return null;
+    }
+
+    public ArrayList<Categoria> getTodos(){
+        return categorias;
     }
 }
